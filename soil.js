bbb6524cd09663e8650f4f91bbf7009add9a07e1
zkc <zkyle12@gmail.com>
2018-02-17 18:22:01 -0700

    Refactor to class structure, add pause

--- soil.js
+++ soil.js
@@ -1,8 +1,12 @@
 
 const main = document.getElementById('main')
 const input = document.getElementById('input')
-const run = document.getElementById("run")
-const reader = document.createElement('div')
+const run = document.getElementById('run')
+const word_display = document.getElementById('word-display')
+const pause = document.getElementById('pause')
+main.removeChild(word_display)
+main.removeChild(pause)
+
 
 input.value = `
 To be, or not to be, that is the question:
@@ -42,33 +46,65 @@ And lose the name of action.
 `
 
 
+class Reader {
+  constructor(word_display) {
+    this.state = {
+      pause: false, 
+      word_index: 0,
+      rate: 500,
+      input_split: null,
+      timeout_id: null,
+    }
+    this.word_display = word_display
+  }
 
+  start(input_split) {
+    this.state.input_split = input_split
+    this.changeWordAndGo()
+  }
+
+  changeWordAndGo() {
+    const { word_display, state: { input_split, rate, timeout_id, word_index }} = this
+    word_display.innerText = input_split[word_index]
+    this.state.word_index++
+    if (word_index < input_split.length) {
+      this.state.timeout_id = setTimeout(() => {
+          this.changeWordAndGo()
+      }, rate)
+    } 
+  }
+
+  pause() {
+    if (this.state.pause) {
+      setTimeout(() => {
+        this.changeWordAndGo()
+      }, this.state.rate)
+      this.state.pause = false
+    } else {
+      clearTimeout(this.state.timeout_id)
+      this.state.pause = true      
+    }
+  }
+}
+
+
+const reader = new Reader(word_display)
+
+pause.onclick = function(e) {
+  reader.pause()
+}
 
 run.onclick = function(e) {
   main.removeChild(input)
-  main.appendChild(reader)
+  main.removeChild(run)
 
-  const rootString = input.value
-  const input_split = rootString.split(' ')
+  main.appendChild(word_display)
+  main.appendChild(pause)
 
-  let word_index = 0
+  const input_split = input.value.split(' ')
 
-  let timeout
-  let rate = 1000
-
-  const changeWordAndGo = () => {
-    reader.innerText = input_split[word_index]
-    word_index++
-
-    timeout = setTimeout(() => {
-      if (word_index < input_split.length) {
-        changeWordAndGo()
-      } 
-    }, rate)
-
-  }
-
-  changeWordAndGo()
-
+  reader.start(input_split)
 }
+
+
 
