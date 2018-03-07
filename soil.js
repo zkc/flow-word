7bb16961e7079d3f9b7d6a157bcdbca985b75f97
zkc <zkyle12@gmail.com>
2018-03-06 22:27:28 -0700

    Pause on start

--- soil.js
+++ soil.js
@@ -7,44 +7,8 @@ const controls = document.getElementById('controls')
 const pop = document.getElementById('pop')
 
 
-input.value = `
-To be, or not to be, that is the question:
-Whether 'tis nobler in the mind to suffer
-The slings and arrows of outrageous fortune,
-Or to take arms against a sea of troubles
-And by opposing end them. To die—to sleep,
-No more; and by a sleep to say we end
-The heart-ache and the thousand natural shocks
-That flesh is heir to: 'tis a consummation
-Devoutly to be wish'd. To die, to sleep;
-To sleep, perchance to dream—ay, there's the rub:
-For in that sleep of death what dreams may come,
-When we have shuffled off this mortal coil,
-Must give us pause—there's the respect
-That makes calamity of so long life.
-For who would bear the whips and scorns of time,
-Th'oppressor's wrong, the proud man's contumely,
-The pangs of dispriz'd love, the law's delay,
-The insolence of office, and the spurns
-That patient merit of th'unworthy takes,
-When he himself might his quietus make
-With a bare bodkin? Who would fardels bear,
-To grunt and sweat under a weary life,
-But that the dread of something after death,
-The undiscovere'd country, from whose bourn
-No traveller returns, puzzles the will,
-And makes us rather bear those ills we have
-Than fly to others that we know not of?
-Thus conscience does make cowards of us all,
-And thus the native hue of resolution
-Is sicklied o'er with the pale cast of thought,
-And enterprises of great pitch and moment
-With this regard their currents turn awry
-And lose the name of action.
-`
-
 const reader = {
-  paused: false,
+  paused: true,
   word_index: 0,
   WPM: 400,
   get rate() {
@@ -65,7 +29,7 @@ const reader = {
     this.text_array = text_array
     this.word_display = word_display
     this.changeWPM(0)
-    this._changeWordAndGo()
+    // this._changeWordAndGo()
   }, 
   _changeWordAndGo() {
     if(this.word_index < this.text_array.length) {
@@ -83,13 +47,13 @@ const reader = {
         this._changeWordAndGo()
       }, this.rate)
       this.paused = false
-      this.unExpandView()
-      document.getElementById('pause-button').innerHTML = 'pause'      
+      document.getElementById('pause-button').innerHTML = 'pause'
+      this.unExpandView()   
     } else {
       clearTimeout(this.timeout_id)
       this.paused = true
-      this.expandView()
       document.getElementById('pause-button').innerHTML = 'play'      
+      this.expandView()
     }
   }, 
   changeWPM(by) {
@@ -162,3 +126,40 @@ document.onclick = (e) => {
       break
   }
 }
+
+// demo text
+input.value = `
+To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles
+And by opposing end them. To die—to sleep,
+No more; and by a sleep to say we end
+The heart-ache and the thousand natural shocks
+That flesh is heir to: 'tis a consummation
+Devoutly to be wish'd. To die, to sleep;
+To sleep, perchance to dream—ay, there's the rub:
+For in that sleep of death what dreams may come,
+When we have shuffled off this mortal coil,
+Must give us pause—there's the respect
+That makes calamity of so long life.
+For who would bear the whips and scorns of time,
+Th'oppressor's wrong, the proud man's contumely,
+The pangs of dispriz'd love, the law's delay,
+The insolence of office, and the spurns
+That patient merit of th'unworthy takes,
+When he himself might his quietus make
+With a bare bodkin? Who would fardels bear,
+To grunt and sweat under a weary life,
+But that the dread of something after death,
+The undiscovere'd country, from whose bourn
+No traveller returns, puzzles the will,
+And makes us rather bear those ills we have
+Than fly to others that we know not of?
+Thus conscience does make cowards of us all,
+And thus the native hue of resolution
+Is sicklied o'er with the pale cast of thought,
+And enterprises of great pitch and moment
+With this regard their currents turn awry
+And lose the name of action.
+`
